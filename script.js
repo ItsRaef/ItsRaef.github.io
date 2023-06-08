ae436056999af44ff4c16186911952504c591524
ItsRaef <89030555+ItsRaef@users.noreply.github.com>
2023-06-08 22:02:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function checkProfile() {
     var profile1 = document.getElementById("profile1");
     var profile2 = document.getElementById("profile2");
 
-    if (profile1.checked || profile2.checked) {
-        window.location.href = "signin.html";
-    } else {
+    if (!profile1.checked && !profile2.checked) {
         alert("Please choose a profile!");
+        return false;
+    }
+
+    return true;
+}
+
+function redirectToSignIn() {
+    if (checkProfile()) {
+        window.location.href = "signin.html";
     }
 }
